fe9c2e256b28aaf92872d4011e548f6323673a8c
Raymond Soffner <raymond.soffner@profilan.nl>
2022-12-12 09:44:05 +0100

    Added missing files

---
+++ ApiManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ApiManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
