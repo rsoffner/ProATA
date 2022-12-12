5cea28f3e2fc189931900c07a77e09700f207532
Raymond Soffner <raymond.soffner@profilan.nl>
2022-12-12 09:08:57 +0100

    Delete APIManager directory

--- APIManager/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ApiManager.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
